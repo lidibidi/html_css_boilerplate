89e15af72a7da2fd652107ae987fd8774acea057
Lidia Santos <Lidia@Lidias-MacBook-Pro.local>
2015-10-29 22:36:28 -0500

    added longer madlib

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,17 +1,19 @@
-
-
 window.onload = function() {
   //alert('hey hey hey');
   statusReport('hey hey hey!');
 
   var name = prompt('What is your name?');
-  var favDrink = prompt('Who is your favorite actor');
-
+  var favdrink = prompt('What is your favorite drink?');
+  var favcolor = prompt('What is your favorite color?');
+  var favday = prompt('What is your favorite day of the week?');
+  var snuggly = prompt('Name something soft and cuddly');
 
   var contentContainer =
   document.getElementById('content');
 
-  contentContainer.innerHTML = name + ' wants ' + character + ' to play them in the movie about their life at General Assembly';
+  contentContainer.innerHTML = ' Once upon a time there was a person named ' + name + ' they enjoyed taking baths in ' + favdrink + ' before they took on the day. ' +
+name + ' wore ' + favcolor + ' underwear outside their pants every ' + favday + ' it helped them cope with their fear of ' +
+snuggly +'.' + ' The end.';
 }
 
 function statusReport(message){
